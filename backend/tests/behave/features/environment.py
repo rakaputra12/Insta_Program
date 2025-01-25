e2f2cc140b9a711ac597eb2af66dfd2974134700
fsk42 <fsk.hartmann@gmail.com>
2025-01-25 13:55:05 +0100

    temp-ordner bei tests automatisch erstellt, falls noch nicht existiert

--- backend/tests/behave/features/environment.py
+++ backend/tests/behave/features/environment.py
@@ -1,6 +1,14 @@
+import os
+
 def before_all(context):
     """Initialisiert Ressourcen für alle Tests."""
+    # Sicherstellen, dass der Temp-Ordner existiert
+    UPLOAD_FOLDER = "./temp"
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+    print("Temp folder created or already exists.")
     print("Starting test suite...")
+
+
 
 def after_all(context):
     """Freigeben der Ressourcen nach allen Tests."""
